ffaaf35a73166e34f081e5ad864ad5b60b7fd7d5
David Baddeley <david.baddeley.nz@gmail.com>
2023-06-29 21:44:03 +1200

    change to using pythreejs for 3D display (and drop mayavi)

---
+++ pyoptic2/_display3d.py
@@ -0,0 +1,161 @@
+#OLD version of display3D, using mayavi. Keeping around for now until STLPart is implemented in new version. 
+
+from .system import *
+from .elements import *
+#from Source import *
+
+import numpy as np
+
+try:
+    #from enthought.tvtk.tools import mlab as ml2
+    from mayavi import mlab
+    #from enthought.tvtk.tools import visual
+    from enthought.tvtk.api import tvtk
+    from enthought.tvtk.common import configure_input_data
+except ImportError:
+    #from tvtk.tools import mlab as ml2
+    #from tvtk.tools import visual
+    from tvtk.api import tvtk
+    from tvtk.common import configure_input_data
+    from mayavi import mlab
+
+
+class STLPart(object):
+    def __init__(self, filename, colour=None, position=[0, 0, 0], orientation=None, rotation=None):
+        self.filename = filename
+        self.reader = tvtk.STLReader(file_name=filename)
+        self.mapper = tvtk.PolyDataMapper()
+        configure_input_data(self.mapper, self.reader.output)
+        self.reader.update()
+        self.actor = tvtk.Actor(mapper=self.mapper)
+        
+        if colour:
+            self.actor.property.color = colour
+        
+        self.actor.position = position
+        if rotation is None:
+            if orientation is None:
+                self.actor.orientation = [0,0,0]
+            else:
+                a2 = -180*np.arctan(orientation[0]/orientation[1])/np.pi
+                a1 = 180*np.arcsin(orientation[2])/np.pi
+                self.actor.orientation = [a1, 0, a2]
+        else:
+            self.actor.orientation = rotation
+    
+    def as_scad(self, origin=[0, 0, 0]):
+        trans = '[%f, %f, %f]' % tuple(np.array(self.actor.position) - np.array(origin))
+        rot = '[%f,%f,%f]' % tuple(self.actor.orientation)
+        return 'translate(%s)rotate(%s)import("%s");\n' % (trans, rot, self.filename)
+    
+    def add_to_scene(self, scene):
+        scene.add_actor(self.actor)
+    
+    @property
+    def position(self):
+        return self.actor.postion
+    
+    @position.setter
+    def set_pos(self, value):
+        self.actor.position = value
+    
+    @property
+    def orientation(self):
+        return self.actor.orientation
+    
+    @orientation.setter
+    def set_or(self, value):
+        self.actor.orientation = value
+
+class Display3D :
+    def __init__(self,s,r, stl_parts=[]):
+        self.s = s
+        self.r = r
+        self.f = mlab.figure()
+        
+        self.stl_parts = stl_parts
+        
+        #if 
+
+        self.e3d = []
+        
+    def _drawLines(self, points, color=[1,1,1]):
+        nrays = points.shape[0]
+        npts = points.shape[2]
+        nsegs = npts - 1
+        lines = np.zeros((nsegs*nrays, 2), 'l')
+        pts_cat = np.zeros([npts*nrays, 3], 'f')
+        for j in range(nrays):
+            pts_cat[(j*npts):((j+1)*npts), :] = points[j,:,:].T
+            lines[(j*nsegs):((j+1)*nsegs),0] = (j*npts) + np.arange(0, nsegs-0.5, 1, 'l')
+            lines[(j * nsegs):((j + 1) * nsegs),1] = (j * npts) + np.arange(1, nsegs + 0.5, 1, 'l')
+            #lines[:,1] = np.arange(1, nsegs+0.5, 1, 'l')
+            
+        d = tvtk.PolyData(points=pts_cat, lines=lines)
+        m = tvtk.PolyDataMapper()
+        configure_input_data(m, d)
+        a = tvtk.Actor(mapper=m)
+        a.property.color = color
+        
+        self.f.scene.add_actor(a)
+        return a
+
+    def Draw(self) :        
+        # loop over optical elements
+        self.f.scene.disable_render = True
+        for e in self.s :
+            x,y,z = e.surface()
+            #print e.name
+            #print z
+            edo = mlab.mesh(x,y,z, color=(.9,1,1), opacity=0.7)
+            #edo.trait_set(representation='wireframe')
+            #edo.use_tubes = False
+            #self.f.add(edo)
+            self.e3d.append(edo)
+            
+        # loop over rays
+        #print self.r
+#        for r in pl.flatten(self.r) :
+#            if r.p1 != None :
+#                print 'line'
+#                print r.p0
+#                print r.p1                
+#                # ray display object
+#
+#                rdo = mlab.Line3([r.p0,r.p1],radius=0.1,representation='wireframe')
+##                rdo.representation = 'wireframe'
+#                self.f.add(rdo)
+#                self.e3d.append(rdo)
+        for rb in self.r:
+            ra = np.concatenate([np.atleast_3d(ri.p0) for ri in rb], 2)
+            #ra = np.array([ri.p0 for ri in ray])
+            if not rb[-1].p1 == None:
+                ra = np.concatenate([ra, np.atleast_3d(rb[-1].p1)], 2)
+            
+            # rbp = [r.p0 for r in rb]
+            # if (not rb[-1].p1 == None):
+            #     rbp.append(rb[-1].p1)
+                
+            if (ra.shape[0] > 1):
+                #print rbp
+                #rdo = ml2.Line3(rbp,radius=0.05, color=r.color)
+    #           rdo.representation = 'wireframe'
+                #rdo.use_tubes = False
+                #self.f.scene.add(rdo)
+                rdo = self._drawLines(ra, color=rb[0].color)
+                self.e3d.append(rdo)
+                
+        for p in self.stl_parts:
+            p.add_to_scene(self.f.scene)
+                
+        self.f.scene.background = (0.38, 0.4, 0.55)
+        self.f.scene.disable_render = False
+
+def Display3DTest() :
+    s = SystemTest()
+    r = s.propagate()
+    d = Display3D(s,r)
+    d.Draw()
+    return d
+
+    
--- pyoptic2/display3d.py
+++ pyoptic2/display3d.py
@@ -1,77 +1,13 @@
-from .system import *
-from .elements import *
-#from Source import *
-
 import numpy as np
-
-try:
-    #from enthought.tvtk.tools import mlab as ml2
-    from mayavi import mlab
-    #from enthought.tvtk.tools import visual
-    from enthought.tvtk.api import tvtk
-    from enthought.tvtk.common import configure_input_data
-except ImportError:
-    #from tvtk.tools import mlab as ml2
-    #from tvtk.tools import visual
-    from tvtk.api import tvtk
-    from tvtk.common import configure_input_data
-    from mayavi import mlab
+import pythreejs as p3j
 
 
-class STLPart(object):
-    def __init__(self, filename, colour=None, position=[0, 0, 0], orientation=None, rotation=None):
-        self.filename = filename
-        self.reader = tvtk.STLReader(file_name=filename)
-        self.mapper = tvtk.PolyDataMapper()
-        configure_input_data(self.mapper, self.reader.output)
-        self.reader.update()
-        self.actor = tvtk.Actor(mapper=self.mapper)
-        
-        if colour:
-            self.actor.property.color = colour
-        
-        self.actor.position = position
-        if rotation is None:
-            if orientation is None:
-                self.actor.orientation = [0,0,0]
-            else:
-                a2 = -180*np.arctan(orientation[0]/orientation[1])/np.pi
-                a1 = 180*np.arcsin(orientation[2])/np.pi
-                self.actor.orientation = [a1, 0, a2]
-        else:
-            self.actor.orientation = rotation
-    
-    def as_scad(self, origin=[0, 0, 0]):
-        trans = '[%f, %f, %f]' % tuple(np.array(self.actor.position) - np.array(origin))
-        rot = '[%f,%f,%f]' % tuple(self.actor.orientation)
-        return 'translate(%s)rotate(%s)import("%s");\n' % (trans, rot, self.filename)
-    
-    def add_to_scene(self, scene):
-        scene.add_actor(self.actor)
-    
-    @property
-    def position(self):
-        return self.actor.postion
-    
-    @position.setter
-    def set_pos(self, value):
-        self.actor.position = value
-    
-    @property
-    def orientation(self):
-        return self.actor.orientation
-    
-    @orientation.setter
-    def set_or(self, value):
-        self.actor.orientation = value
-
-class Display3D :
-    def __init__(self,s,r, stl_parts=[]):
+class Display3D(object):
+    def __init__(self,s,r, parts=[]):
         self.s = s
         self.r = r
-        self.f = mlab.figure()
         
-        self.stl_parts = stl_parts
+        self._parts = parts
         
         #if 
 
@@ -79,77 +15,136 @@ class Display3D :
         
     def _drawLines(self, points, color=[1,1,1]):
         nrays = points.shape[0]
-        npts = points.shape[2]
-        nsegs = npts - 1
-        lines = np.zeros((nsegs*nrays, 2), 'l')
-        pts_cat = np.zeros([npts*nrays, 3], 'f')
-        for j in range(nrays):
-            pts_cat[(j*npts):((j+1)*npts), :] = points[j,:,:].T
-            lines[(j*nsegs):((j+1)*nsegs),0] = (j*npts) + np.arange(0, nsegs-0.5, 1, 'l')
-            lines[(j * nsegs):((j + 1) * nsegs),1] = (j * npts) + np.arange(1, nsegs + 0.5, 1, 'l')
-            #lines[:,1] = np.arange(1, nsegs+0.5, 1, 'l')
-            
-        d = tvtk.PolyData(points=pts_cat, lines=lines)
-        m = tvtk.PolyDataMapper()
-        configure_input_data(m, d)
-        a = tvtk.Actor(mapper=m)
-        a.property.color = color
         
-        self.f.scene.add_actor(a)
-        return a
-
-    def Draw(self) :        
-        # loop over optical elements
-        self.f.scene.disable_render = True
-        for e in self.s :
-            x,y,z = e.surface()
-            #print e.name
-            #print z
-            edo = mlab.mesh(x,y,z, color=(.9,1,1), opacity=0.7)
-            #edo.trait_set(representation='wireframe')
-            #edo.use_tubes = False
-            #self.f.add(edo)
-            self.e3d.append(edo)
+        color = f'rgb({int(255*color[0])}, {int(255*color[1])}, {int(255*color[2])})'
+        o = []
+        
+        for j in range(nrays):
+            o.append(p3j.Line(geometry=p3j.BufferGeometry(attributes={'position': p3j.BufferAttribute(points[j,:,:].T)}),
+                              material=p3j.LineBasicMaterial(color=color)))
             
+        return o
+
+    def _material(self, color, transparent=False, opacity=1.0, specular='#afafff', shininess=30):
+        material = p3j.MeshPhongMaterial()
+        #material = p3.CustomMaterial("standard")
+        material.color = color
+        material.clipping = True
+        material.side = "DoubleSide"
+        material.polygonOffset = True
+        material.polygonOffsetFactor = 1
+        material.polygonOffsetUnits = 1
+        material.transparent = transparent
+        material.opacity = opacity
+        material.specular = specular
+        material.shininess = shininess
+        material.alpha = opacity
+        #material.update("metalness", 0.3)
+        #material.update("roughness", 0.8)
+        return material
+    
+    def _calc_normals(self, verts):
+        v0 = verts[0::3, :] - verts[1::3, :] + np.array([0.1, 0, 0])
+        v1 = verts[0::3, :] - verts[2::3, :] + np.array([0, 0.1, 0])
+        n = np.cross(v0, v1)
+        n = n / np.sqrt(np.sum(n**2, axis=1))[:, np.newaxis]
+
+        n = np.repeat(n.astype('f4'), 3, axis=0)
+
+        return n
+    
+    def Draw(self, shape=(1000, 600)) :        
+        # loop over optical elements
+
+        elements = []
+
+        bbox = np.array([-1, 1, -1, 1, -1, 1]) # x0, x1, y0, y1, z0, z1
+
+        for e in self.s :
+            verts = e.surface_triangles()
+            #print(verts.shape)
+
+            if (verts.shape[0] == 0):
+                continue
+
+            bbox[0] = min(bbox[0], np.min(verts[:,0]))
+            bbox[1] = max(bbox[1], np.max(verts[:,0]))
+            bbox[2] = min(bbox[2], np.min(verts[:,1]))
+            bbox[3] = max(bbox[3], np.max(verts[:,1]))
+            bbox[4] = min(bbox[4], np.min(verts[:,2]))
+            bbox[5] = max(bbox[5], np.max(verts[:,2]))
+
+            
+            if e.material.type == e.material.REFLECT:
+                m = self._material('silver', specular='#efefff', shininess=90)
+            else:
+                m = self._material('lightblue', opacity=0.8, shininess=20)
+                                                      
+            
+            mesh = p3j.Mesh(geometry=p3j.BufferGeometry(attributes={'position': p3j.BufferAttribute(verts), 
+                                                                   'index': p3j.BufferAttribute(np.arange(verts.shape[0], dtype='uint32')),
+                                                                   'normal': p3j.BufferAttribute(self._calc_normals(verts))
+                                                                   }),
+                            material=m)
+
+            elements.append(mesh)
+
+        #print(elements)
+
         # loop over rays
-        #print self.r
-#        for r in pl.flatten(self.r) :
-#            if r.p1 != None :
-#                print 'line'
-#                print r.p0
-#                print r.p1                
-#                # ray display object
-#
-#                rdo = mlab.Line3([r.p0,r.p1],radius=0.1,representation='wireframe')
-##                rdo.representation = 'wireframe'
-#                self.f.add(rdo)
-#                self.e3d.append(rdo)
         for rb in self.r:
             ra = np.concatenate([np.atleast_3d(ri.p0) for ri in rb], 2)
-            #ra = np.array([ri.p0 for ri in ray])
             if not rb[-1].p1 == None:
                 ra = np.concatenate([ra, np.atleast_3d(rb[-1].p1)], 2)
-            
-            # rbp = [r.p0 for r in rb]
-            # if (not rb[-1].p1 == None):
-            #     rbp.append(rb[-1].p1)
                 
             if (ra.shape[0] > 1):
-                #print rbp
-                #rdo = ml2.Line3(rbp,radius=0.05, color=r.color)
-    #           rdo.representation = 'wireframe'
-                #rdo.use_tubes = False
-                #self.f.scene.add(rdo)
+                bbox[0] = min(bbox[0], np.min(ra[:,:,0]))
+                bbox[1] = max(bbox[1], np.max(ra[:,:,0]))
+                bbox[2] = min(bbox[2], np.min(ra[:,:,1]))
+                bbox[3] = max(bbox[3], np.max(ra[:,:,1]))
+                bbox[4] = min(bbox[4], np.min(ra[:,:,2]))
+                bbox[5] = max(bbox[5], np.max(ra[:,:,2]))
+
                 rdo = self._drawLines(ra, color=rb[0].color)
-                self.e3d.append(rdo)
-                
-        for p in self.stl_parts:
-            p.add_to_scene(self.f.scene)
-                
-        self.f.scene.background = (0.38, 0.4, 0.55)
-        self.f.scene.disable_render = False
+                elements.extend(rdo)
+
+        
+        # loop over parts (things which are responsible for drawing themselves, typically optomechanics with a STL or STEP description)
+        # these should either already be a pythreejs object, or have a to_threejs() method which returns one
+        for p in self._parts:
+            if hasattr(p, 'to_threejs'):
+                elements.append(p.to_threejs())
+            else:
+                elements.append(p)
+        
+        # create camera, light, and scene
+        key_light = p3j.DirectionalLight(color='white', position=[3, 5, 1], intensity=0.5)
+
+        box_centre = (bbox[0::2] + bbox[1::2]) / 2
+
+        c = p3j.CombinedCamera(position=tuple(box_centre + np.array([0, 0, 100])), up=[1, 0, 0], children=[key_light], 
+                            width=shape[0], height=shape[1], mode='orthographic')
+        c.up=[1, 0, 0]
+        c.right=[0, 1, 0]
+        c.position = tuple(box_centre + np.array([0, 0, 100]))
+        
+        c.target = tuple(box_centre)
+
+        
+        s = p3j.Scene(children=elements + [c, p3j.AmbientLight(color='#777777')], background='#eeeeee')
+        renderer = p3j.Renderer(camera=c,
+                            scene=s,
+                            alpha=True,
+                            clearOpacity=0,
+                            controls=[p3j.OrbitControls(controlling=c)],
+                            width=shape[0], height=shape[1])
+        
+        return renderer
+                        
+        
 
 def Display3DTest() :
+    from pyoptic2.system import SystemTest
     s = SystemTest()
     r = s.propagate()
     d = Display3D(s,r)
